cf8d70cff16565570f4e4c414db261a0e2a540a3
sicuni <sicuni@qq.com>
2022-09-27 15:04:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Alertmanager Dingtalk Webhook
+
+Webhook service support send Prometheus 2.0 alert message to Dingtalk.
+
+## How To Use
+
+将alertmanager的webhook指向该服务，并且在prometheus中配置对应的规则需要上报钉钉的URL。具体的匹配可以按照alertmanager的alertmanagerConfig配置
+
+
+```
+groups:
+alert:test-load-1
+expr:node_load1 > 1
+for: 2m
+labels:
+    dingTalk: https://oapi.dingtalk.com/robot/send?access_token=xxxxx
+annotations:
+description: {{$labels.instance}}: job {{$labels.job}} 测试测试 负载大于1
+summary: {{$labels.instance}}: load1 >1
+```
---
+++ webhook.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"github.com/sicuni/alertmanaer-dingtalk-webhook/model"
+	"github.com/sicuni/alertmanaer-dingtalk-webhook/notifier"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	h            bool
+	defaultRobot string
+	listen		 string
+)
+
+func init() {
+	flag.BoolVar(&h, "h", false, "help")
+	flag.StringVar(&defaultRobot, "defaultRobot", "", "global dingtalk robot webhook, you can overwrite by alert rule with annotations dingtalkRobot")
+	flag.StringVar(&listen, "listen", ":9998", "server listen port")
+}
+
+func main() {
+
+	flag.Parse()
+
+	if h {
+		flag.Usage()
+		return
+	}
+
+	router := gin.Default()
+	router.POST("/webhook", func(c *gin.Context) {
+		var notification model.Notification
+
+		err := c.BindJSON(&notification)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		notifyDingTalk := notification.CommonLabels["dingTalk"]
+		err = notifier.Send(notification, notifyDingTalk)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "send to dingtalk successful!"})
+
+	})
+	router.Run(listen)
+}
